3b0f52b53c89c76ae97c1457d8960ce27c69c2c3
misaminic <misaminic@gmail.com>
2025-10-13 21:11:06 +0200

    use react query in digest

--- src/pages/Digest.tsx
+++ src/pages/Digest.tsx
@@ -1,15 +1,36 @@
 import { usePreferencesStore } from '../store/usePreferencesStore'
-import { useNewsStore } from '../store/newsStore'
+import { useQuery } from '@tanstack/react-query'
 import type { PreferencesState } from '../store/usePreferencesStore'
+import { fetchArticles } from '../api/api'
+
 
 export default function Digest() {
   const topics = usePreferencesStore((state: PreferencesState) => state.topics)
-  const { articles, fetchNews } = useNewsStore()
+
+  
+  const { data: articles, isLoading, isError, error } = useQuery({
+    queryKey: ["articles", topics], // cache per set of topics
+    queryFn: () => fetchArticles(topics),
+    enabled: topics.length > 0,     // don’t fetch if no topics selected
+    staleTime: 5 * 60 * 1000,       // 5 minutes fresh
+  });
+
+  if (isLoading) {
+   return <p>Loading articles...</p>
+  }
+
+  if (isError) {
+    return <p className="text-red-600">Error: {(error as Error).message}</p>
+  } 
+
+  if (!articles || articles.length === 0) {
+    return <p>No articles found.</p>
+  } 
 
   return (
     <div>
       <h2>Digest Page</h2>
-      <button onClick={() => fetchNews(topics)}>Fetch News</button>
+      {/* <button onClick={() => fetchNews(topics)}>Fetch News</button> */}
       
       <ul>
         {articles?.map((a, i) => (
@@ -17,7 +38,7 @@ export default function Digest() {
             <a href={a.url} target="_blank" rel="noopener noreferrer">
               {a.title}
             </a>{' '}
-            ({a.source})
+            ({a.source.name})
           </li>
         ))}
       </ul>
